60eba77520e5b11ef1cdea154a4311836a257dad
Coderbat <mihirmittal6@gmail.com>
2024-04-27 22:56:21 +0100

    change in menus

--- main.gd
+++ main.gd
@@ -1,28 +1,16 @@
-extends Node2D
+extends Control
 
-var play_button = Button.new()
-var quit_button = Button.new()
+@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Button1 as Button
+@onready var quit_button = $MarginContainer/HBoxContainer/VBoxContainer/Button2 as Button
+@onready var start_level = preload("res://World.tscn")
 
 func _ready():
-	play_button.text = "Play"
-	play_button.set_size(Vector2(100, 50))
-	play_button.connect("pressed", _on_play_button_pressed)
-	add_child(play_button)
-
-	quit_button.text = "Quit"
-	quit_button.set_size(Vector2(100, 50))
-	quit_button.connect("pressed", _on_quit_button_pressed)
-	add_child(quit_button)
-
-	await get_viewport().size_changed
-	_position_buttons()
-
-func _position_buttons():
-	play_button.set_position(Vector2(get_viewport().size.x / 2 - play_button.get_size().x / 2, get_viewport().size.y / 2 - play_button.get_size().y * 2))
-	quit_button.set_position(Vector2(get_viewport().size.x / 2 - quit_button.get_size().x / 2, get_viewport().size.y / 2))
-
-func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://World.tscn")
-
-func _on_quit_button_pressed():
+	start_button.button_down.connect(on_start_pressed)
+	quit_button.button_down.connect(on_exit_pressed)
+	
+	
+func on_start_pressed():
+	get_tree().change_scene_to_packed(start_level)
+	
+func on_exit_pressed():
 	get_tree().quit()
